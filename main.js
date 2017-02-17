78e53f12a7a7a9e50e8cceb9b9a190eb2c79087f
Austin Dudas <dudasaus@umich.edu>
2017-02-17 18:02:54 -0500

    Full login flow

--- main.js
+++ main.js
@@ -70,19 +70,27 @@ open(gmUrl);
 var main = {
     state: 'startUp',
     users: [],
-    user: [],
+    user: '',
     groups: [],
     groupId: -1,
+    token: -1,
     msgId: 0,
+    server: {},
     actions: new actionEmitter(),
     step: function() {
         if (this.state == 'startUp') {
             startUp(this);
         }
+        else if (this.state == 'addUser') {
+            addUser(this);
+        }
+        else if (this.state == 'getUserInfo') {
+            getUserInfo(this);
+        }
         else if (this.state == 'selectUser') {
             selectUser(this);
         }
-        else if (this.state == 'gettingGroups') {
+        else if (this.state == 'getGroups') {
             getGroups(this);
         }
         else if (this.state == 'selectGroup') {
@@ -93,12 +101,15 @@ var main = {
 
 // Emitters for handling async stuff
 
+// Occurs when you've loaded the users from local data
 main.actions.once('startUp', (arr) => {
     main.users = arr;
     main.state = 'selectUser';
     main.step();
 });
-main.actions.once('selectUser', (num) => {
+
+// Occurs when input for the select user screen is given
+main.actions.on('selectUser', (num) => {
     num = parseInt(num);
     if (isNaN(num) || num < 0 || num > main.users.length) {
         console.log('Invalid value');
@@ -113,13 +124,48 @@ main.actions.once('selectUser', (num) => {
             main.user = main.users[num - 1];
             main.state = 'getUserInfo';
         }
+        main.step();
     }
 });
+
+// Occurs when the server attempts to add a user
+main.actions.on('addUser', (token) => {
+    if (token == -1) {
+        console.log("Failed to authenticate new user");
+        main.state = 'selectUser';
+    }
+    else {
+        main.token = token;
+        console.log("Token: " + token);
+        main.state = 'getUserInfo';
+    }
+    main.server.close();
+    main.step();
+});
+
+// Occurs when we try to get the user info
+main.actions.on('getUserInfo', (name) => {
+    if (name == -1) {
+        console.log("Authentication failed.");
+        main.state = 'selectUser';
+    }
+    else {
+        main.users.push([name, main.token]);
+        main.user = name;
+        main.state = 'getGroups'
+        console.log("Welcome, " + name);
+    }
+    main.step();
+});
+
+// Occurs once the groups info has loaded from the https request
 main.actions.once('getGroups', (arr) => {
     main.groups = arr;
     main.state = 'selectGroup';
     main.step();
 });
+
+// Occurs when input for the select gorup screen is given
 main.actions.on('selectGroup', (num) => {
     num = parseInt(num);
     if (isNaN(num) || num < 0 || num >= main.groups.length) {
@@ -135,6 +181,8 @@ main.actions.on('selectGroup', (num) => {
         main.step();
     }
 });
+
+// Occurs when input is given when messaging a group
 main.actions.on('sendMessages', (msg) => {
     if (msg + '' == ".exit\n") {
         process.exit(0);
@@ -168,12 +216,83 @@ function startUp(m) {
 // USERS
 
 function selectUser(m) {
+    console.log(m.state);
     console.log('(0) Add new user');
     for (var i = 0; i < m.users.length; i++) {
         console.log(`(${i + 1}) ${m.users[i][0]}`);
     }
 
     console.log('Select a user by entering the choice number');
+}
+
+function addUser(m) {
+    // Port for the server
+    var PORT = 3000;
+
+    // Server
+    main.server = http.createServer( (request, response) => {
+        if (request.url.substr(0,10) == "/groupnode") {
+            var info = qparse.parse(request.url);
+
+            // Check if we got the token
+            if (info['access_token']) {
+                var tempToken = info['access_token'];
+                // console.log("Token: " + token);
+                var html = fs.readFileSync('success.html');
+                response.end(html);
+                m.actions.emit('addUser', tempToken);
+            }
+            else {
+                var html = fs.readFileSync('error.html');
+                response.end(html);
+                m.actions.emit('addUser', -1);
+            }
+
+        }
+        else {
+            response.end("404");
+            // m.actions.emit('addUser', -1);
+        }
+
+    });
+
+    // Start listening
+    main.server.listen(PORT);
+
+    // Open the GroupMe URL
+    var gmUrl = 'https://oauth.groupme.com/oauth/authorize?client_id=ZpEe5Yl2VnZAAOomwQ3SZFJ2TCsnGbKi51waJTZeQQZ6pSL5';
+    open(gmUrl);
+
+}
+
+function getUserInfo(m) {
+
+    var resultString = '';
+
+    var options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/users/me?token=' + m.token,
+        method: 'GET'
+    }
+
+    var req = https.request(options, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            resultString += chunk;
+        });
+        res.on('end', () => {
+            var response = JSON.parse(resultString).response;
+            if (response) {
+                m.actions.emit('getUserInfo', response.name);
+            }
+            else {
+                m.actions.emit('getUserInfo', -1);
+            }
+        });
+    });
+
+    req.end();
+
 }
 
 // END USERS
@@ -188,7 +307,7 @@ function getGroups(m) {
 
     var options = {
         hostname: 'api.groupme.com',
-        path: '/v3/groups?token=' + token,
+        path: '/v3/groups?token=' + m.token,
         method: 'GET'
     }
 
@@ -222,6 +341,9 @@ function selectGroup(m) {
 // MESSAGING
 
 function sendMessage(m, msg) {
+
+    var resultString = ''
+
     // Data to post
     var data = JSON.stringify({
         'message': {
@@ -232,7 +354,7 @@ function sendMessage(m, msg) {
 
     var options = {
         hostname: 'api.groupme.com',
-        path: '/v3/groups/' + m.groupId + '/messages?token=' + token,
+        path: '/v3/groups/' + m.groupId + '/messages?token=' + m.token,
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -243,10 +365,10 @@ function sendMessage(m, msg) {
     var req = https.request(options, (res) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
-            console.log("BODY: " + chunk);
+            resultString += chunk;
         });
         res.on('end', () => {
-            console.log('no more data');
+            // console.log('no more data');
         });
     });
     req.on('error', (e) => {
